25f9a0f052ca2534dd3a2b327da7d622d33f3904
Bernard Chisumo <bchisumo74@yahoo.com>
2017-01-03 16:07:48 +0000

    Uploading a User Image

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -41,6 +41,11 @@
         'as'  => 'account.save'
     ]);
 
+    Route::get('/userimage/{filename}', [
+       'uses'  => 'UserController@getUserImage',
+        'as'   =>'account.image'
+    ]);
+
     Route::get('/dashboard', [
         'uses' =>'PostController@getDashboard',
         'as'   =>'dashboard',
